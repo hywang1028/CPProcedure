83b49e7182b6638afd8dc7c2ba838d743f8db85d
marshal <marshalye888@gmail.com>
2012-01-12 09:24:28 +0800

    modify NotMatchBranchOffice SP

--- BizDeptReport/Proc_NotMatchedBranchOffice.sql
+++ BizDeptReport/Proc_NotMatchedBranchOffice.sql
@@ -5,7 +5,7 @@ end
 go
 
 create procedure Proc_NotMatchedBranchOffice
-	@Type nvarchar(5)= N'商城'
+	@Type nvarchar(10)= N'非银联银商商户地区'
 as
 begin
 
@@ -17,7 +17,7 @@ end
 	
 	
 --2.Get Ums&UnionPay Not Matched BranchOffice
-if @Type = N'银联银商'
+if @Type = N'银联银商分支机构'
 begin
 
 select
@@ -44,7 +44,7 @@ end
 
 
 --3.Get Not Matched BranchOffice Whose Channel is CP
-if(@Type = N'CP')
+if(@Type = N'其他分支机构')
 begin
 
 select distinct
@@ -55,15 +55,15 @@ select distinct
 from
 	Table_SalesDeptConfiguration
 where
-	Channel = N'CP'
+	Channel not in (N'银联',N'银商')
 	and
 	ISNULL(BranchOffice,N'') <> N'';
-	
+
 end
 
 
 --4.Get Not Matched BranchOffice From Emall Data
-if(@Type = N'商城')
+if(@Type = N'商城分支机构')
 begin
 
 select
@@ -81,8 +81,31 @@ where
 	ISNULL(BranchOfficeNameRule.UnnormalBranchOfficeName,N'') = N''
 	and
 	isnull(EmallTransSum.BranchOffice, N'') not in (N'ChinaPay', N'Chinova', N'Shopex');
-
-
+	
 end		
+		
+--5.Get Not Matched BranchOffice From CP Area
+if(@Type = N'非银联银商商户地区')	
+begin
+
+select
+	N'销售部' as DataSupport,
+	SalesDeptConfiguration.Area as BranchOffice,
+	SalesDeptConfiguration.MerchantName,
+	SalesDeptConfiguration.MerchantNo
+from
+	Table_SalesDeptConfiguration SalesDeptConfiguration
+	left join
+	Table_BranchOfficeNameRule BranchOfficeNameRule
+	on
+		SalesDeptConfiguration.Area = BranchOfficeNameRule.BranchOfficeShortName
+where
+	BranchOfficeNameRule.BranchOfficeShortName is null
+	and
+	SalesDeptConfiguration.Channel not in (N'银联',N'银商')
+	and
+	BranchOfficeNameRule.BranchOfficeShortName not in (N'数字王府井',N'好易联');
 			
+end
+
 end
