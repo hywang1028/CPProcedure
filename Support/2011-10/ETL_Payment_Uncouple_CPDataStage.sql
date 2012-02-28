5b2cfc49ce10ad0fb31eb3761ca6cf12a602918d
DBFocus <shadow_wu82@163.com>
2012-02-28 17:39:14 +0800

    Add xp_GetParentProcs

---
+++ Support/2011-10/ETL_Payment_Uncouple_CPDataStage.sql
@@ -0,0 +1,14 @@
+
+--**Backup CPDataStage
+use CPDataStage
+
+--1.
+drop table Incoming_GateBankRelation;
+
+drop table Table_GateBankRelation;
+
+
+--2.
+drop table Incoming_MerchantGroupRelation;
+
+drop table Table_MerchantGroupRelation
