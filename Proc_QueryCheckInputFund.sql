d9fafe85fdc9a1de346fb24946612f74a80487fe
DingJunHao <djhlsl@126.com>
2012-09-24 16:47:50 +0800

    Add Proc_QueryCheckInputFunds

---
+++ Proc_QueryCheckInputFund.sql
@@ -0,0 +1,49 @@
+IF OBJECT_ID(N'Proc_QueryCheckInputFunds',N'P') is not null
+begin
+	drop procedure Proc_QueryCheckInputFunds
+end
+go
+
+create procedure Proc_QueryCheckInputFunds
+	@StartDate datetime = '2012-08-29',
+	@EndDate datetime = '2012-09-10'
+as
+begin
+
+
+--1. Check input
+if (@StartDate is null or  @EndDate is null)
+begin
+	raiserror(N'Input params cannot be empty in Proc_QueryCheckInputFunds', 16, 1)
+end
+
+
+--2. Prepare StartDate and EndDate
+declare @CurrEndDate datetime;
+set @CurrEndDate = DATEADD(DAY,1,@EndDate);
+
+
+--3.Prepare All Data
+select
+	GateNo,
+	SUM(TransCnt) TransCnt,
+	SUM(TransAmt)/100.0 TransAmt
+from
+	Table_ReconFileInfo
+where
+	UploadDate >= @StartDate
+	and
+	UploadDate < @CurrEndDate
+group by
+	GateNo;
+
+
+end
+
+
+
+
+
+
+
+
